ef4bbd0c19e6df76bf851aa0f8d9ccbf8ec274e0
Arisodee <arisodee@gmail.com>
2020-12-10 19:52:24 +0300

    Initial commit

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Desktop/MOVIE BEST/virtual"
+set -gx VIRTUAL_ENV "/home/ariso/Documents/Python/Movie-best/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
